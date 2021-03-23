097d0217871e6f4286b6ae03ab3316210a10e200
vernehar <verne.hartikainen@gmail.com>
2021-03-23 18:32:21 +0200

    added /add and /help commands

--- twitterbotv2.py
+++ twitterbotv2.py
@@ -3,8 +3,9 @@ from tweepy import OAuthHandler
 from tweepy import API
 import json
 import time
+import os
 
-def twitterupdate():
+def twitterapi():
     print("updating..")
     #read twitter api credentials and declare them to variables
     creds = [line.rstrip() for line in open('credentials.txt')]
@@ -20,6 +21,14 @@ def twitterupdate():
 
     api = tweepy.API(auth,wait_on_rate_limit=True)
     print(api.rate_limit_status)
+    
+    return api
+
+
+def twitterupdate():
+
+    api = twitterapi()
+
     #find users
     influencers = []
     influencers = open("influencers.txt","r").readlines()
@@ -29,6 +38,7 @@ def twitterupdate():
         print(j)
         user = api.get_user(influencers[j].rstrip())
         print(api.rate_limit_status)
+
         try:
             userfile = open(influencers[j].rstrip() + ".txt","r")
         except IOError:
@@ -64,3 +74,49 @@ def twitterupdate():
             newFollows.update({influencers[j].rstrip(): changeloglist})
     
     return newFollows
+
+
+def checkTwitterUser(userName, api):
+    try:
+        print("OK1")
+        api.get_user(userName)
+
+        currentInfluencers = [line.rstrip() for line in open('influencers.txt')]
+        print(currentInfluencers)
+        if userName not in currentInfluencers:
+            print("OK2")
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def appendUserToList(userName):
+    api = twitterapi()
+    if checkTwitterUser(userName, api):
+        try:
+            open("influencers.txt","a").writelines(userName + "\n")
+
+            newInfluencer = api.get_user(userName)
+
+            newInfluencerFile = open(userName + ".txt","w")
+            newInfluencerNewFollowings = open(userName + "newfollowings.txt","w")
+
+            for friend in newInfluencer.friends():
+                newInfluencerFile.writelines(friend.screen_name + "\n")
+                newInfluencerNewFollowings.writelines(friend.screen_name + "\n")
+            
+            newInfluencerFile.close()
+            newInfluencerNewFollowings.close()
+            return True
+        except:
+            return False
+    else:
+        return False
+
+def getCurrentInfluencers():
+    currentInfluencers = [line.rstrip() for line in open('influencers.txt')]
+    influencerString = "\n"
+    for i in range(len(currentInfluencers)):
+        influencerString = influencerString + currentInfluencers[i] + "\n"
+    return influencerString
